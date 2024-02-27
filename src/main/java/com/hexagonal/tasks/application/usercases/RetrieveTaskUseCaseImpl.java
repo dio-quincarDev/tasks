656501819a3d3be4d37173f374df18c1258c7ee2
dio-quincarDev <dio-quincar@outlook.com>
2024-02-26 19:53:27 -0500

    Repo de Arq. Hexagonal, post 201, get 405

---
+++ src/main/java/com/hexagonal/tasks/application/usercases/RetrieveTaskUseCaseImpl.java
@@ -0,0 +1,26 @@
+package com.hexagonal.tasks.application.usercases;
+
+import com.hexagonal.tasks.domain.models.Task;
+import com.hexagonal.tasks.domain.ports.in.RetrieveTaskUseCase;
+import com.hexagonal.tasks.domain.ports.out.TaskRepositoryPort;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RetrieveTaskUseCaseImpl implements RetrieveTaskUseCase {
+    private final TaskRepositoryPort taskRepositoryPort;
+
+    public RetrieveTaskUseCaseImpl(TaskRepositoryPort taskRepositoryPort) {
+        this.taskRepositoryPort = taskRepositoryPort;
+    }
+
+    @Override
+    public Optional<Task> getTask(Long id) {
+        return taskRepositoryPort.findbyId(id);
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepositoryPort.findAll();
+    }
+}
